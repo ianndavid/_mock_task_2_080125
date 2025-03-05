672e797b884b16a5223c3c4debe68c6b1f53324b
ianndavid <147523373+ianndavid@users.noreply.github.com>
2025-03-05 12:37:53 +0000

    now

--- website/static/index.js
+++ website/static/index.js
@@ -1,28 +1,34 @@
 
-                              // Get today's date
-                              const today = new Date();
-                              const yyyy = today.getFullYear();
-                              const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-                              const dd = String(today.getDate()).padStart(2, '0');
+ // Get today's date
+const today = new Date();
+const yyyy = today.getFullYear();
+const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+const dd = String(today.getDate()).padStart(2, '0');
+// Format today's date as YYYY-MM-DD
+ const todayFormatted = `${yyyy}-${mm}-${dd}`;
                       
-                              // Format today's date as YYYY-MM-DD
-                              const todayFormatted = `${yyyy}-${mm}-${dd}`;
-                      
-                              // Calculate the date 5 days from today
-                              const maxDate = new Date(today);
-                              maxDate.setDate(maxDate.getDate() + 400);
-                              const maxYyyy = maxDate.getFullYear();
-                              const maxMm = String(maxDate.getMonth() + 1).padStart(2, '0');
-                              const maxDd = String(maxDate.getDate()).padStart(2, '0');
-                              const maxDateFormatted = `${maxYyyy}-${maxMm}-${maxDd}`;
+ // Calculate the date 5 days from today
+const maxDate = new Date(today);
+maxDate.setDate(maxDate.getDate() + 400);
+const maxYyyy = maxDate.getFullYear();
+const maxMm = String(maxDate.getMonth() + 1).padStart(2, '0');
+const maxDd = String(maxDate.getDate()).padStart(2, '0');
+const maxDateFormatted = `${maxYyyy}-${maxMm}-${maxDd}`;
                       
                               // Set the min and max attributes for the date input
-                              const dateInput = document.getElementById('Hotel_Check_in');
-                              dateInput.setAttribute('min', todayFormatted);
-                              dateInput.setAttribute('max', maxDateFormatted);
+const dateInput = document.getElementById('Hotel_Check_in');
+dateInput.setAttribute('min', todayFormatted);
+dateInput.setAttribute('max', maxDateFormatted);
 
-                              const dateInput2 = document.getElementById('Hotel_Check_Out');
-                              dateInput2.setAttribute('min', todayFormatted);
-                              dateInput2.setAttribute('max', maxDateFormatted);
+const dateInput2 = document.getElementById('Hotel_Check_Out');
+dateInput2.setAttribute('min', todayFormatted);
+dateInput2.setAttribute('max', maxDateFormatted);
 
-                              
+                               
+
+
+function changefont() {
+    const fontchange = document.getElementById('change-font');
+    fontchange.style.fontFamily = "Arial, sans-serif";
+}
+
